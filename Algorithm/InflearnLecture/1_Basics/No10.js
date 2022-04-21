df1c6bbec156fd2f048311ae30b54cb6fd884c81
eazae <xynezae@gmail.com>
2022-04-21 18:40:31 +0900

    solved: INFLEC_basics_10

--- Algorithm/InflearnLecture/1_Basics/No10.js
+++ Algorithm/InflearnLecture/1_Basics/No10.js
@@ -9,14 +9,14 @@ function solution(s, t) {
 
 /* 답안 */
 //? 내장 함수 사용
-function solution(s, t) {
-  // t를 구분자로 문자열을 배열로 반환함
-  let answer = s.split(t);
+// function solution(s, t) {
+//   // t를 구분자로 문자열을 배열로 반환함
+//   let answer = s.split(t);
 
-  // 마지막에 구분자가 들어가있어도, 빈 문자열의 원소로 들어가진다. (편리)
-  console.log(answer); // [ 'COMPUTE', 'P', 'OG', 'AMMING', '', '', '' ]
-  return answer.length - 1;
-}
+//   // 마지막에 구분자가 들어가있어도, 빈 문자열의 원소로 들어가진다. (편리)
+//   console.log(answer); // [ 'COMPUTE', 'P', 'OG', 'AMMING', '', '', '' ]
+//   return answer.length - 1;
+// }
 
 // 입력예제
 console.log(solution('COMPUTERPROGRAMMING', 'R'));
