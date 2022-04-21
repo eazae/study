fa02f44e2ef747afcd7273cbe66b384da8a4a63e
eazae <xynezae@gmail.com>
2022-04-21 18:40:38 +0900

    solved: INFLEC_basics_11

--- Algorithm/InflearnLecture/1_Basics/No11.js
+++ Algorithm/InflearnLecture/1_Basics/No11.js
@@ -10,20 +10,20 @@ function solution(s) {
 }
 
 /* 답안 */
-function solution(s) {
-  let answer = 0;
-  for (let x of s) {
-    // i) ASCII 코드로 변환해주는 함수
-    //! let num=x.charCodeAt();
-    //if(num>=65 && num<=90) answer++;
+// function solution(s) {
+//   let answer = 0;
+//   for (let x of s) {
+//     // i) ASCII 코드로 변환해주는 함수
+//     //! let num=x.charCodeAt();
+//     //if(num>=65 && num<=90) answer++;
 
-    // ii)
-    //* 대문자 변환 후와 전이 같다는 것은, 이미 대문자였다는 것이므로
-    if (x === x.toUpperCase()) answer++;
-  }
+//     // ii)
+//     //* 대문자 변환 후와 전이 같다는 것은, 이미 대문자였다는 것이므로
+//     if (x === x.toUpperCase()) answer++;
+//   }
 
-  return answer;
-}
+//   return answer;
+// }
 
 // 입력예제
 console.log(solution('KoreaTimeGood'));
