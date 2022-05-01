63cdc44ffbb48ff6c7b7638beefd8d88a10f70fb
eazae <xynezae@gmail.com>
2022-05-01 20:32:36 +0900

    solved: INFLEC_string_05

--- Algorithm/InflearnLecture/3_String/No05.js
+++ Algorithm/InflearnLecture/3_String/No05.js
@@ -18,21 +18,21 @@ function solution(str) {
 }
 
 /* 답안 */
-function solution(s) {
-  let answer = '';
-  let cnt = 1;
-  // 마지막 문자를 비교하기 위해서 빈 문자를 추가
-  s = s + ' ';
-  for (let i = 0; i < s.length - 1; i++) {
-    if (s[i] === s[i + 1]) cnt++;
-    else {
-      answer += s[i];
-      if (cnt > 1) answer += String(cnt);
-      cnt = 1;
-    }
-  }
-  return answer;
-}
+// function solution(s) {
+//   let answer = '';
+//   let cnt = 1;
+//   // 마지막 문자를 비교하기 위해서 빈 문자를 추가
+//   s = s + ' ';
+//   for (let i = 0; i < s.length - 1; i++) {
+//     if (s[i] === s[i + 1]) cnt++;
+//     else {
+//       answer += s[i];
+//       if (cnt > 1) answer += String(cnt);
+//       cnt = 1;
+//     }
+//   }
+//   return answer;
+// }
 
 // 입력예제
 console.log(solution('KKHSSSSSSSE'));
