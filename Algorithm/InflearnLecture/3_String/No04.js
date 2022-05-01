39a6687cefa5646b3b4e1c6e7b1dee8f94f6e63f
eazae <xynezae@gmail.com>
2022-05-01 20:32:31 +0900

    solved: INFLEC_string_04

--- Algorithm/InflearnLecture/3_String/No04.js
+++ Algorithm/InflearnLecture/3_String/No04.js
@@ -37,29 +37,29 @@ function solution(s, t) {
 }
 
 /* 답안 */
-// O(n): for문을 -> <- 이렇게 2번만 돌면 됨
-function solution(s, t) {
-  let answer = [];
-  let p = 1000;
-  for (let x of s) {
-    if (x === t) {
-      p = 0;
-      answer.push(p);
-    } else {
-      p++;
-      answer.push(p);
-    }
-  }
-  p = 1000;
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] === t) p = 0;
-    else {
-      p++;
-      answer[i] = Math.min(answer[i], p);
-    }
-  }
-  return answer;
-}
+//? O(n): for문을 -> <- 이렇게 2번만 돌면 됨
+// function solution(s, t) {
+//   let answer = [];
+//   let p = 1000;
+//   for (let x of s) {
+//     if (x === t) {
+//       p = 0;
+//       answer.push(p);
+//     } else {
+//       p++;
+//       answer.push(p);
+//     }
+//   }
+//   p = 1000;
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     if (s[i] === t) p = 0;
+//     else {
+//       p++;
+//       answer[i] = Math.min(answer[i], p);
+//     }
+//   }
+//   return answer;
+// }
 
 // 입력예제
 console.log(solution('teachermode', 'e'));
