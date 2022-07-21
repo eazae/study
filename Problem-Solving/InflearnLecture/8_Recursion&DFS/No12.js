e9e9f5c1c4f02757f8ce1bc4dcf7ad32729023be
eazae <xynezae@gmail.com>
2022-07-21 14:31:21 +0900

    solved: INFLEC_Recursion-DFS_12

--- Problem-Solving/InflearnLecture/8_Recursion&DFS/No12.js
+++ Problem-Solving/InflearnLecture/8_Recursion&DFS/No12.js
@@ -23,6 +23,18 @@ function solution(N, R) {
 }
 
 /* 답안 */
+// function solution(n, r) {
+//   let answer = [];
+//   let dy = Array.from(Array(35), () => Array(35).fill(0));
+
+//   function DFS(n, r) {
+//     if (dy[n][r] > 0) return dy[n][r];
+//     if (n === r || r === 0) return 1;
+//     else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+//   }
+//   answer = DFS(n, r);
+//   return answer;
+// }
 
 // 입력예제
 console.log(solution(5, 3));
